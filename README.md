f046370efacf0c0337c81d1d2b6a00b047161795
Gaurav Luthra <luthra.zenith@gmail.com>
2017-04-19 11:55:01 +0100

    Provided support to get capital name for states and strict option is provided

--- README.md
+++ README.md
@@ -1,18 +1,18 @@
-# us-states-util
-A rich library that provides various utility functions to get/check name or USPS code of states and much more
+# states-utils
+A rich library that provides various utility functions to get/check name or USPS code or state capital of states and much more
 
 ### Installation
 
 Install with [npm](https://www.npmjs.com/):
 
 ```sh
-$ npm install us-states-util --save
+$ npm install states-utils --save
 ```
 
 ### Usage
 
 ```javascript
-var STATE_UTILS = require('us-states-util');
+var STATE_UTILS = require('states-utils');
 
 STATE_UTILS.isUSAState('CA');
 //=> true
@@ -27,18 +27,20 @@ STATE_UTILS.getUSPSCode('New York');
 
 ### Clone the repo
 
-git clone https://github.com/luthraG/us-states-util.git
+git clone https://github.com/luthraG/states-utils.git
 
 ### API
 
-#### isUSAState(state)
+#### isUSAState(key, strict)
 
-This API takes one parameter i.e. state which could either be state name or USPS code. It returns boolean value indicating if provided value if valid US state
+This API takes two parameters 
+1. key <String> 	: It could either be state name or USPS code. It returns boolean value indicating if provided value if valid US state
+2. strict<Boolean>  : It is an optional parameter which when set to true then it does not check for associated states, inhabited territories and federal districts rather it only checks 50 US states
 
 **Example**
 
 ```javascript
-var STATE_UTILS = require('us-states-util');
+var STATE_UTILS = require('states-utils');
 
 
 // USPS code in upper case
@@ -69,45 +71,89 @@ STATE_UTILS.isUSAState('New Delhi');
 STATE_UTILS.isUSAState();
 //=> false
 
+// Check for an associated state
+STATE_UTILS.isUSAState('Marshall Islands');
+//=> true
+
+// With strict parameter set to true
+STATE_UTILS.isUSAState('Marshall Islands', true);
+//=> false
+
+// Check for an inhabited territory
+STATE_UTILS.isUSAState('Guam');
+//=> true
+
+// With strict parameter set to true
+STATE_UTILS.isUSAState('Guam', true);
+//=> false
+
 ```
 
-#### getStates()
+#### getStates(strict)
 
 This API returns all the state names of The united States of America.
+1. It takes an optional parameter - strict which when set to true returns only 50 state names of USA else it returns name of associated states, inhabited territories and federal districts as well besides 50 states of USA
 
 **Example**
 
 ```javascript
-var STATE_UTILS = require('us-states-util');
+var STATE_UTILS = require('states-utils');
 
 
 STATE_UTILS.getStates();
 //=> ['Alaska', 'Alabama', ......, 'Texas', ...]
 
+// When strict parameter is not present
+STATE_UTILS.getStates().length;
+//=> 59
+
+// When strict parameter is set to false
+STATE_UTILS.getStates(false).length;
+//=> 59
+
+// When strict parameter is set to true
+STATE_UTILS.getStates(true).length;
+//=> 50
+
 ```
 
-#### getUSPSCodes()
+#### getUSPSCodes(strict)
 
 This API returns the USPS codes for all the states of The united States of America.
+1. It takes an optional parameter - strict which when set to true returns only 50 state USPS codes else it returns USPS codes of associated states, inhabited territories and federal districts as well besides 50 states of USA
 
 **Example**
 
 ```javascript
-var STATE_UTILS = require('us-states-util');
+var STATE_UTILS = require('states-utils');
 
 STATE_UTILS.getUSPSCodes();
 //=> ['AK', 'AL', ......, 'TX', ...]
 
+// When strict parameter is not present
+STATE_UTILS.getUSPSCodes().length;
+//=> 59
+
+// When strict parameter is set to false
+STATE_UTILS.getUSPSCodes(false).length;
+//=> 59
+
+// When strict parameter is set to true
+STATE_UTILS.getUSPSCodes(true).length;
+//=> 50
+
 ```
 
-#### getStateName()
+#### getStateName(key, strict)
 
-Given the USPS code, this API returns the state name for a valid US state.
+Given the USPS code or state capital, this API returns the state name for a valid US state. It takes two parameters:
+1. key <String> 	: It could either be state capital name or USPS code.
+2. strict<Boolean>  : It is an optional parameter which when set to true then it does not check for associated states, inhabited territories and federal districts rather it only checks 50 US states
 
 **Example**
 
 ```javascript
-var STATE_UTILS = require('us-states-util');
+var STATE_UTILS = require('states-utils');
 
 
 // USPS code in upper case
@@ -118,35 +164,98 @@ STATE_UTILS.getStateName('TX');
 STATE_UTILS.getStateName('ca');
 //=> 'California'
 
+// USPS code with strict set to false for an inhabited territory
+STATE_UTILS.getStateName('GU', false);
+//=> 'Guam'
+
+// USPS code with strict set to true for an inhabited territory
+STATE_UTILS.getStateName('GU', true);
+//=> undefined
+
 // Invalid USPS code
 STATE_UTILS.getStateName('GL');
 //=> undefined
 
+// A valid capital name
+STATE_UTILS.getStateName('Phoenix');
+//=> 'Arkansas'
+
+// An invalid capital name
+STATE_UTILS.getStateName('ca');
+//=> undefined
+
+// Capital name with strict set to false for an inhabited territory state
+STATE_UTILS.getStateName('Pago Pago', false);
+//=> 'American Samoa'
+
+// Capital name with strict set to true for an inhabited territory state
+STATE_UTILS.getStateName('Pago Pago', true);
+//=> undefined
+
 ```
 
-#### getUSPSCode()
+#### getUSPSCode(key, strict)
 
 Given the state name, this API returns the USPS code for a valid US state.
+1. key <String> 	: It could either be state capital name or state name.
+2. strict<Boolean>  : It is an optional parameter which when set to true then it does not check for associated states, inhabited territories and federal districts rather it only checks 50 US states
 
 **Example**
 
 ```javascript
-var STATE_UTILS = require('us-states-util');
+var STATE_UTILS = require('states-utils');
 
 // A valid State name
 STATE_UTILS.getUSPSCode('Mississippi');
 //=> 'MS'
 
 // State name in lower case
-STATE_UTILS.getStateName('massachusetts');
+STATE_UTILS.getUSPSCode('massachusetts');
 //=> 'MA'
 
 // State name in upper case
-STATE_UTILS.getStateName('NEW YORK');
+STATE_UTILS.getUSPSCode('NEW YORK');
 //=> 'NY'
 
+// State capital name
+STATE_UTILS.getUSPSCode('Pierre');
+//=> 'SD'
+
 // Invalid State name
-STATE_UTILS.getStateName('Delhi');
+STATE_UTILS.getUSPSCode('Delhi');
+//=> undefined
+
+```
+
+#### getStateCapital(key, strict)
+
+Given the state name or USPS code, this API returns the state capital for a valid US state.
+1. key <String> 	: It could either be state USPS code or state name.
+2. strict<Boolean>  : It is an optional parameter which when set to true then it does not check for associated states, inhabited territories and federal districts rather it only checks 50 US states
+
+**Example**
+
+```javascript
+var STATE_UTILS = require('states-utils');
+
+// A valid State name
+STATE_UTILS.getStateCapital('Mississippi');
+//=> 'Jackson'
+
+// A valid USPS code
+STATE_UTILS.getStateCapital('CO');
+//=> 'Denver'
+
+// State name for an associated state
+STATE_UTILS.getStateCapital('Northern Mariana Islands');
+//=> 'Saipan'
+
+// State name for an associated state with strict set to true
+STATE_UTILS.getStateCapital('Northern Mariana Islands', true);
+//=> undefined
+
+// Invalid State name
+STATE_UTILS.getStateCapital('Delhi');
 //=> undefined
 
 ```
--- index.js
+++ index.js
@@ -1,136 +1,144 @@
-var NODE_RATIFY                 = require('node-ratify');
+// Local Includes
+var STATES_DATA                                 = require(__dirname + '/data/statesData.js');
 
-var US_STATE_NAME_TO_CODE       = {
-        'Alaska'                                                : 'AK',
-        'Alabama'                                               : 'AL',
-        'American Samoa'                                        : 'AS',
-        'Armed Forces Americas (except Canada)'                 : 'AA',
-        'Armed Forces Europe, Middle East, Africa, and Canada'  : 'AE',
-        'Armed Forces Pacific'                                  : 'AP',
-        'Arkansas'                                              : 'AR',
-        'Arizona'                                               : 'AZ',
-        'California'                                            : 'CA',
-        'Colorado'                                              : 'CO',
-        'Connecticut'                                           : 'CT',
-        'District of Columbia'                                  : 'DC',
-        'Delaware'                                              : 'DE',
-        'Federated States of Micronesia'                        : 'FM',
-        'Florida'                                               : 'FL',
-        'Georgia'                                               : 'GA',
-        'Guam'                                                  : 'GU',
-        'Hawaii'                                                : 'HI',
-        'Iowa'                                                  : 'IA',
-        'Idaho'                                                 : 'ID',
-        'Illinois'                                              : 'IL',
-        'Indiana'                                               : 'IN',
-        'Kansas'                                                : 'KS',
-        'Kentucky'                                              : 'KY',
-        'Louisiana'                                             : 'LA',
-        'Marshall Islands'                                      : 'MH',
-        'Massachusetts'                                         : 'MA',
-        'Maryland'                                              : 'MD',
-        'Maine'                                                 : 'ME',
-        'Michigan'                                              : 'MI',
-        'Minnesota'                                             : 'MN',
-        'Missouri'                                              : 'MO',
-        'Mississippi'                                           : 'MS',
-        'Montana'                                               : 'MT',
-        'Northern Mariana Islands'                              : 'MP',
-        'North Carolina'                                        : 'NC',
-        'North Dakota'                                          : 'ND',
-        'Nebraska'                                              : 'NE',
-        'New Hampshire'                                         : 'NH',
-        'New Jersey'                                            : 'NJ',
-        'New Mexico'                                            : 'NM',
-        'Nevada'                                                : 'NV',
-        'New York'                                              : 'NY',
-        'Ohio'                                                  : 'OH',
-        'Oklahoma'                                              : 'OK',
-        'Oregon'                                                : 'OR',
-        'Palau'                                                 : 'PW',
-        'Pennsylvania'                                          : 'PA',
-        'Puerto Rico'                                           : 'PR',
-        'Rhode Island'                                          : 'RI',
-        'South Carolina'                                        : 'SC',
-        'South Dakota'                                          : 'SD',
-        'Tennessee'                                             : 'TN',
-        'Texas'                                                 : 'TX',
-        'Utah'                                                  : 'UT',
-        'Virginia'                                              : 'VA',
-        'Virgin Islands'                                        : 'VI',
-        'Vermont'                                               : 'VT',
-        'Washington'                                            : 'WA',
-        'Wisconsin'                                             : 'WI',
-        'West Virginia'                                         : 'WV',
-        'Wyoming'                                               : 'WY',
-    },
+// System Includes
+var NODE_RATIFY                                 = require('node-ratify');
 
-    US_STATE_CODE_TO_NAME       = { },
+var US_STATE_NAME_TO_CODE                       = STATES_DATA['US_STATE_NAME_TO_CODE'],
+    US_STATE_CODE_TO_NAME                       = { },
+    US_INHABITED_TERRITORIES_NAME_TO_CODE       = STATES_DATA['US_INHABITED_TERRITORIES_NAME_TO_CODE'],
+    US_INHABITED_TERRITORIES_CODE_TO_NAME       = { },
+    US_STATE_NAME_TO_CAPITAL                    = STATES_DATA['US_STATE_NAME_TO_CAPITAL'],
+    US_CAPITAL_TO_STATE_NAME                    = { },
+    US_INHABITED_TERRITORIES_NAME_TO_CAPITAL    = STATES_DATA['US_INHABITED_TERRITORIES_NAME_TO_CAPITAL'],
+    US_CAPITAL_TO_INHABITED_TERRITORY_NAME      = { };
 
-    US_STATE_NAME_UPPER_TO_CODE = { };
+var STATE_NAME_STRICT                           = [ ],
+    STATE_NAME_ALL                              = [ ],
+    STATE_CODE_STRICT                           = [ ],
+    STATE_CODE_ALL                              = [ ];
 
-function _capitalize () {
-    (Object.keys(US_STATE_NAME_TO_CODE) || []).forEach(function(stateName) {
-        US_STATE_NAME_UPPER_TO_CODE[stateName.toUpperCase()] = US_STATE_NAME_TO_CODE[stateName];
-    });
-};
+//
+// Create reverse mapping
+//
+function _reverseAndPrecompute () {
+    STATE_NAME_STRICT = Object.keys(US_STATE_NAME_TO_CODE);
 
-function _reverse () {
-    (Object.keys(US_STATE_NAME_TO_CODE) || []).forEach(function(stateName) {
+    var territoryKeys = Object.keys(US_INHABITED_TERRITORIES_NAME_TO_CODE);
+
+    (STATE_NAME_STRICT || []).forEach(function(stateName) {
         US_STATE_CODE_TO_NAME[US_STATE_NAME_TO_CODE[stateName]] = stateName;
+        US_CAPITAL_TO_STATE_NAME[US_STATE_NAME_TO_CAPITAL[stateName]] = stateName;
     });
+
+    (territoryKeys || []).forEach(function(territoryName) {
+        US_INHABITED_TERRITORIES_CODE_TO_NAME[US_INHABITED_TERRITORIES_NAME_TO_CODE[territoryName]] = territoryName;
+        US_CAPITAL_TO_INHABITED_TERRITORY_NAME[US_INHABITED_TERRITORIES_NAME_TO_CAPITAL[territoryName]] = territoryName;
+    });
+
+    // Let us precompute now
+    STATE_CODE_STRICT   = Object.keys(US_STATE_CODE_TO_NAME);
+    STATE_NAME_ALL      = (STATE_NAME_STRICT || []).concat(territoryKeys);
+    STATE_CODE_ALL      = (STATE_CODE_STRICT || []).concat(Object.keys(US_INHABITED_TERRITORIES_CODE_TO_NAME));
 };
+
+function _toTitleCase(str) {
+    if (str && NODE_RATIFY.isString(str)) {
+        str = str.trim();
+        if (str.length > 2)
+            return str.replace(/\b\w+/g, function(txt) {
+                return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+            });
+        else
+            return str.toUpperCase();
+    }
+
+    return null;
+}
 
 //
 // Is given state code or state name is a valid state
 //
-function isUSAState (state) {
-    state = state && NODE_RATIFY.isString(state) ? state.toUpperCase() : null;
-    return (US_STATE_CODE_TO_NAME[state] || US_STATE_NAME_UPPER_TO_CODE[state]) ? true : false;
+function isUSAState (state, strict) {
+    state = state && NODE_RATIFY.isString(state) ? _toTitleCase(state) : null;
+    var topLevel = (US_STATE_CODE_TO_NAME[state] || US_STATE_NAME_TO_CODE[state]);
+
+    if(strict) {
+        return topLevel ? true : false;
+    } else {
+        return (US_INHABITED_TERRITORIES_CODE_TO_NAME[state] || US_INHABITED_TERRITORIES_NAME_TO_CODE[state] || topLevel) ? true : false;
+    }
 };
 
 //
 // Get all the USPS code
 //
-function getUSPSCodes() {
-    return Object.keys(US_STATE_CODE_TO_NAME);
+function getUSPSCodes(strict) {
+    return strict ? STATE_CODE_STRICT : STATE_CODE_ALL;
 };
 
 //
 // Get name of all the states
 //
-function getStates() {
-    return Object.keys(US_STATE_NAME_TO_CODE);
+function getStates(strict) {
+    return strict ? STATE_NAME_STRICT : STATE_NAME_ALL;
 };
 
 //
-// Given a state USPS Code it finds the state name
+// Given a state USPS Code or state capital it finds the state name
 //
-function getStateName(stateCode) {
-    stateCode = stateCode && NODE_RATIFY.isString(stateCode) ? stateCode.toUpperCase() : null;
+function getStateName(searchKey, strict) {
+    searchKey = searchKey && NODE_RATIFY.isString(searchKey) ? _toTitleCase(searchKey) : null;
+    var topLevel = (US_STATE_CODE_TO_NAME[searchKey] || US_CAPITAL_TO_STATE_NAME[searchKey]);
 
-    return US_STATE_CODE_TO_NAME[stateCode];
+    if(strict) {
+        return topLevel;
+    } else {
+        return (US_INHABITED_TERRITORIES_CODE_TO_NAME[searchKey] || US_CAPITAL_TO_INHABITED_TERRITORY_NAME[searchKey] || topLevel);
+    }
 };
 
 //
-// Given a state name it finds the USPS Code
+// Given a state name or state capital it finds the USPS Code
 //
-function getUSPSCode(state) {
-    state = state && NODE_RATIFY.isString(state) ? state.toUpperCase() : null;
+function getUSPSCode(searchKey, strict) {
+    searchKey = searchKey && NODE_RATIFY.isString(searchKey) ? _toTitleCase(searchKey) : null;
+    var topLevel = (US_STATE_NAME_TO_CODE[searchKey] || US_STATE_NAME_TO_CODE[US_CAPITAL_TO_STATE_NAME[searchKey]]);
 
-    return US_STATE_NAME_UPPER_TO_CODE[state];
+    if(strict) {
+        return topLevel;
+    } else {
+        searchKey = (searchKey === 'HagåTñA') ? 'Hagatna' : searchKey;
+        return (US_INHABITED_TERRITORIES_NAME_TO_CODE[searchKey] ||
+                US_INHABITED_TERRITORIES_NAME_TO_CODE[US_CAPITAL_TO_INHABITED_TERRITORY_NAME[searchKey]] ||
+                topLevel);
+    }
 };
 
-// Let us create list of state name and state abbreviation with state name in capital case
-_capitalize();
+//
+// Given a state name or state USPS code it finds the state capital
+//
+function getStateCapital(searchKey, strict) {
+    searchKey = searchKey && NODE_RATIFY.isString(searchKey) ? _toTitleCase(searchKey) : null;
+    var topLevel = (US_STATE_NAME_TO_CAPITAL[searchKey] || US_STATE_NAME_TO_CAPITAL[US_STATE_CODE_TO_NAME[searchKey]]);
+
+    if(strict) {
+        return topLevel;
+    } else {
+        return (US_INHABITED_TERRITORIES_NAME_TO_CAPITAL[searchKey] ||
+                US_INHABITED_TERRITORIES_NAME_TO_CAPITAL[US_INHABITED_TERRITORIES_CODE_TO_NAME[searchKey]] ||
+                topLevel);
+    }
+};
 
 // Let us create state abbreviation and state name mapping
-_reverse();
+_reverseAndPrecompute();
 
 exports = module.exports = {
     isUSAState      : isUSAState,
     getStates       : getStates,
     getUSPSCodes    : getUSPSCodes,
     getStateName    : getStateName,
-    getUSPSCode     : getUSPSCode
+    getUSPSCode     : getUSPSCode,
+    getStateCapital : getStateCapital
 };
